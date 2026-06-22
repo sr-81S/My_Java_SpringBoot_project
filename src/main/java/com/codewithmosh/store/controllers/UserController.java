3ab07462e1bf9faac088c543a415fdea05524821
saroj sabar <sarojsabar12345@gmail.com>
2026-06-22 23:22:42 +0530

    data valication done on request DTO and add error handeler on the user controller according to the missing field

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -7,13 +7,17 @@ import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -54,7 +58,7 @@ public class UserController {
     //get data from the request body so we can create the new record
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody RegisterUserRequest request, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody RegisterUserRequest request, UriComponentsBuilder uriBuilder) {
         var user = userMapper.toEntity(request);
         System.out.println(user);
         userRepository.save(user);
@@ -100,6 +104,18 @@ public class UserController {
         user.setPassword(request.getNewPassword());
         userRepository.save(user);
         return ResponseEntity.noContent().build();
+    }
+
+
+    //Execption handeler
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        var error = new HashMap<String, String>();
+        ex.getBindingResult().getFieldErrors().forEach((errorField) -> {
+            error.put(errorField.getField(), errorField.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(error);
     }
 
 }
--- src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
@@ -1,10 +1,21 @@
 package com.codewithmosh.store.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterUserRequest {
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must be less than 255 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 25, message = "Password must be at least 6 characters")
     private String password;
 }
