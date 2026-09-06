636f3e0d082aac18d0be386c9147ee68362d2e58
Saroj Sabar <sarojsabar12345@gmail.com>
2026-09-06 17:32:55 +0530

    the spring security added and costom filter added

---
+++ src/main/java/com/codewithmosh/store/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.codewithmosh.store.filters;
+
+import com.codewithmosh.store.services.JwtServices;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtServices jwtServices;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        var token = authHeader.replace("Bearer ", "");
+
+        if(!jwtServices.validateToken(token)){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        var authentication = new UsernamePasswordAuthenticationToken(jwtServices.getEmailFromToken(token), null, null);
+        // Set the authentication in the security context
+        // SecurityContextHolder.getContext().setAuthentication(authentication);
+        authentication.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+
+    }
+
+}
--- src/main/java/com/codewithmosh/store/services/JwtServices.java
+++ src/main/java/com/codewithmosh/store/services/JwtServices.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -32,15 +33,26 @@ public class JwtServices {
     //method for validate toke
     public Boolean validateToken(String token) {
         try {
-           var claims = Jwts.parser()
-                    .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
-                    .build()
-                    .parseSignedClaims(token)
-                   .getPayload();
+            var claims = getClaims(token);
 
             return claims.getExpiration().after(new Date());
         } catch (JwtException e) {
             return false;
         }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                 .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                 .build()
+                 .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    //Function for get email from token
+    public String getEmailFromToken(String token) {
+        var claims = getClaims(token);
+
+        return claims.getSubject();
     }
 }
