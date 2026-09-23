8ba46444fc62bf700bcccb61d40fb5fb470e8df1
Saroj Sabar <sarojsabar12345@gmail.com>
2026-09-23 23:58:54 +0530

    the final check out end point created

---
+++ src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CheckoutResponse {
+
+    private Long orderId;
+
+
+}
